5d38a943af3060092e93889b1640140772a30dae
Smuggler@Fury <mattsadov@gmail.com>
2019-11-23 14:15:47 +1000

    I've been told that code will be ready later. Move to something else

--- QUTMS_BMS_V3/src/MCP2517FD.c
+++ QUTMS_BMS_V3/src/MCP2517FD.c
@@ -6,10 +6,12 @@
  */ 
 #include "MCP2517FD.h"
 
-void MCP2517FD_reg_write() {
+void MCP2517FD_reg_write(uint8_t reg_address, uint8_t reg_value) {
 	// unset to start transmission. Critical Section.	
 	WRITE_BIT(MCP2517FD_PORT_CS, MCP2517FD_PIN_CS, LOW);
-	SPI_send_byte(0x00);
+	SPI_send_byte(MCP2515_WRITE);
+	SPI_send_byte(reg_address);
+	SPI_send_byte(reg_value);
 	// Set back.
 	WRITE_BIT(MCP2517FD_PORT_CS, MCP2517FD_PIN_CS, HIGH);
 	// Make sure that extra time is needed
--- QUTMS_BMS_V3/src/MCP2517FD.h
+++ QUTMS_BMS_V3/src/MCP2517FD.h
@@ -12,7 +12,11 @@
 #define MCP2517FD_PORT_CS		PORTC
 #define MCP2517FD_PIN_CS		PINC7		//***
 
-
-
+#define MCP2517FD_RESET       0x00
+#define MCP2517FD_READ        0x03
+#define MCP2517FD_WRITE		0x02		//*** send this, then send address byte, then send data
+#define MCP2517FD_READ_CRC    0x0B
+#define MCP2517FD_WRITE_CRC   0x0A
+#define MCP2517FD_WRITE_SAFE  0x0C
 
 #endif /* MCP2517FD_H_ */
